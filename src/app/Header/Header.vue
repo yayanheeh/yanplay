083fe9fd8f82504e84da6252b82a4a60bb0f5fd8
yayanheeh <tokoxp58@gmail.com>
2019-07-02 13:34:26 +0700

    Update Header.vue

--- src/app/Header/Header.vue
+++ src/app/Header/Header.vue
@@ -1,5 +1,9 @@
 
 <template>
+
+     <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.1/material.indigo-pink.min.css">
+       
+
 <!--hide mobile-->
   <v-toolbar class="class="hidden-md-and-up"
     color="vTyoobRed"
@@ -60,60 +64,15 @@
   </v-toolbar>
 <!--hide mobile-->
 <!--hide destktop-->
-<v-toolbar class="class="hidden-md-and-up"
-    color="vTyoobRed"
-    dense
-    fixed
-    dark
-    clipped-left
-    :height="64"
-    app
-  >
-    <v-toolbar-side-icon @click="toggleDrawer"/>
-    <router-link 
-      @click.native="sendLogoClickGA"
-      aria-label="home" 
-      :to="{name: 'home'}">
-      <b>Yplay</b>
-    </router-link>
-
-    <v-layout 
-      row 
-      justify-center>
-      <v-flex 
-        xs12 
-        md8 
-        lg6 >
-        <v-form 
-          @submit.prevent="searchVideos">
-          <v-combobox
-            ref="searchBox"
-            :menu-props="{
-              transition: 'slide-y-transition'
-            }"
-            clearable
-            class="mx-3"
-            no-filter
-            :loading="searching"
-            :items="searchSuggestions"
-            :append-icon="null"
-            :search-input.sync="query"
-            v-model="searchSelectedValue"
-            @input="searchVideos"
-            dark
-            flat
-            full-width
-            prepend-inner-icon="search"
-            hide-no-data
-            hide-details
-            :label="$t('searchHeaderPlaceholder')"
-            solo-inverted
-          />
-        </v-form>
-      </v-flex>
-    </v-layout>
-
-  </v-toolbar>
+<div class="mdl-cell--hide-phone mdl-cell--hide-tablet alert desktop">
+          You are on a desktop
+       </div>
+       <div class="mdl-cell--hide-desktop mdl-cell--hide-tablet alert phone">
+          You are on a phone
+       </div>
+       <div class="mdl-cell--hide-desktop mdl-cell--hide-phone alert tablet">
+          You are on a tablet
+       </div>
 
 
 </template>
