b98f993e23c530799e5e4db059bc39dd72002b0a
yayanheeh <tokoxp58@gmail.com>
2019-07-02 17:41:00 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -28,9 +28,9 @@ if (fs.existsSync("key.pem") && fs.existsSync("cert.pem")) {
 module.exports = {
   runtimeCompiler: false,
   pwa: {
-    name: "vTyoob",
-    themeColor: "#cb0000",
-    msTileColor: "#cb0000",
+    name: "Yplay",
+    themeColor: "#4385f3",
+    msTileColor: "#4385f3",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black-translucent",
     iconPaths: {
